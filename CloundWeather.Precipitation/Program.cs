333c1f40346a7cd08ede52db5d259d90f87efbe1
Rizwan Murtaza <rizwan.murtaza@ca.vu>
2024-07-17 15:37:06 +0100

    Intial Commit

---
+++ CloundWeather.Precipitation/Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+var app = builder.Build();
+
+
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
